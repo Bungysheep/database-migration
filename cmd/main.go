dc2bfdbce3ffae3fe68370f75426858d5d2e0f63
James Embongbulan <james.embongbulan@gmail.com>
2019-11-23 07:50:16 +0800

    Added database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,25 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/bungysheep/database-migration/pkg/protocol/db"
+	_ "github.com/lib/pq"
+)
 
 func main() {
 	if err := migrate(); err != nil {
 		log.Fatalf("ERROR: %v", err)
+		os.Exit(1)
 	}
 }
 
 func migrate() error {
+	_, err := db.OpenDbConn()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
